778bb259532364ac982f6ef71feca494ee1271f6
soufo.25 <nteyouyousoufo>
2022-06-23 10:12:00 +0200

    my react

--- src/App.js
+++ src/App.js
@@ -1,24 +1,17 @@
-import logo from './logo.svg';
-import './App.css';
+import React from "react";
+import "./App.css";
+import NavbarComp from './Components/NavbarComp';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className ="App">
+      <NavbarComp />
+      <h1>Hello world</h1>
+      <p>Welcome to your favorite Coffee  Shop</p>
+      <h6>Enjoy Your drink</h6>
+    
     </div>
+    
   );
 }
 
